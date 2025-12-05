aa9bf43893feac097077325dae08528b8cd8cca6
olha36 <olhakucheruk36@gmail.com>
2025-12-05 20:21:38 +0200

    add cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,20 +1,22 @@
-export default [
-  "strapi::logger",
+module.exports = [
   "strapi::errors",
   "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: [
+        "https://bright-luck-8845c17382.strapiapp.com",
+        "https://weather-forecast-cyan-one.vercel.app/",
+        "http://localhost:3000",
+      ],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+    },
+  },
   "strapi::poweredBy",
+  "strapi::logger",
   "strapi::query",
   "strapi::body",
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
-  {
-    name: "strapi::cors",
-    config: {
-      origin: ["http://localhost:3000", "https://your-frontend.com"],
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      keepHeaderOnError: true,
-    },
-  },
 ];
